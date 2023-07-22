be82c2b52fc4c6e650214a8ec8799f84a71f3ba7
Andrea Ciceri <andrea.ciceri@autistici.org>
2023-07-22 15:53:06 +0000

    new contract

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,22 +1,22 @@
 pragma solidity =0.8.19;
 
-interface IInputBox {
-    function addInput(
-        bytes calldata _input
-    ) external returns (bytes32);
-}
+import "@cartesi/rollups@0.8.2/contracts/interfaces/IInput.sol";
 
 contract Test {
     address constant cartesiDapp = 0xF8C694fd58360De278d5fF2276B7130Bfdc0192A;
-    address constant cartesiInputFacet = 0xeA8538B194742b992B19e694C13D63120908880e;
+    string public s; 
 
     event ItWorks(string message);
     
     function cartesiCallback(string calldata message) external {
+        s = message;
         emit ItWorks(message);
     }
 
     function foo() external {
-        IInputBox(cartesiInputFacet).addInput(bytes('{"address":address(this), "body":"7+2"}'));
+        bytes memory payload = abi.encode(address(this), "numpy.");
+        eval("python", "fdfd");
+
+        IInput(cartesiDapp).addInput(payload);
     }
 }
